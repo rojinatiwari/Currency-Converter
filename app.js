7dfb75f8f834e2b96f87f1ad0c05f5d4687756d8
rojinatiwari <rojinatiwari17@gmail.com>
2024-07-09 21:35:06 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const BASE_URL = "https://api.fastforex.io/fetch-one?api_key=910caf49aa-dafd434a3f-sgch5r&from=USD&to=NPR";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "NPR") {
+      newOption.selected = "selected";
+    }
+    // add 
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `https://api.fastforex.io/fetch-one?api_key=910caf49aa-dafd434a3f-sgch5r&from=${fromCurr.value}&to=${toCurr.value}`;
+  // Storing promise in response variable
+  let response = await fetch(URL);
+
+  let data = await response.json();
+  let rate = data.result[toCurr.value];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+
+// load in window 
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
